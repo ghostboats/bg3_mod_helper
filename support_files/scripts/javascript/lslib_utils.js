f1407d6e41f6896d6595837da37218cc2b0ad1bc
Ken Bynell <ken.bynell@gmail.com>
2024-04-17 21:02:11 -0500

    nowhere near done but LSLIB.dll can load in now

---
+++ support_files/scripts/javascript/lslib_utils.js
@@ -0,0 +1,38 @@
+const koffi = require('koffi');
+const fs = require('fs');
+const { getConfig }  = require('../../config.js');
+const { divinePath } = getConfig();
+const LSLIB_DLL = `\\LSLIB.dll`;
+const TOOL_DIR = `\\Tools` + LSLIB_DLL;
+
+
+function LOAD_LSLIB()
+{
+    var LSLIB_PATH;
+    var LSLIB;
+
+    if (fs.existsSync(divinePath + LSLIB_DLL))
+    {
+        LSLIB_PATH = divinePath + LSLIB_DLL;
+    }
+    else if (fs.existsSync(divinePath + TOOL_DIR))
+    {
+        LSLIB_PATH = divinePath + TOOL_DIR;
+    }
+    else
+    {
+        console.error("LSLib.dll not found at " + divinePath + ".")
+        LSLIB = null;
+        return;
+    }
+
+    if (LSLIB_PATH)
+    {
+        LSLIB = koffi.load(LSLIB_PATH);
+    }
+
+    return LSLIB;
+}
+
+
+module.exports = { LOAD_LSLIB }
--- support_files/scripts/javascript/xml_to_loca.js
+++ support_files/scripts/javascript/xml_to_loca.js
@@ -1,15 +1,28 @@
+const vscode = require('vscode')
 const koffi = require('koffi')
 const path = require('path');
+const LSLIB = require('./lslib_utils')
 
 const { getConfig }  = require('../../config.js');
-const { divinePath } = getConfig()
+const { divinePath } = getConfig();
+//const LSLIB = require('./lslib_utils')
 
 function testing()
 {
     console.log("help");
-    console.log(divinePath)
+    console.log(divinePath);
+
+    console.log(LSLIB);
 }
 
-module.exports = { testing }
+function convert(outputPath, file)
+{
+    
+}
+
+module.exports = 
+{ 
+    testing, convert
+};
 //const lslib = require(divinePath + '/LSLIB.dll')
 
