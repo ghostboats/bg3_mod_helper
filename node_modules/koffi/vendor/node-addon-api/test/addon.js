133355a4ce0395c57974b02bfa92642be3663cc6
Ken Bynell <ken.bynell@gmail.com>
2024-04-25 14:50:40 -0500

    removed unneeded koffi package

--- node_modules/koffi/vendor/node-addon-api/test/addon.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = require('./common').runTestInChildProcess({
-  suite: 'addon',
-  testName: 'workingCode',
-  expectedStderr: ['TestAddon::~TestAddon']
-});
--- node_modules/koffi/vendor/node-addon-api/test/objectwrap_function.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = require('./common').runTestInChildProcess({
-  suite: 'objectwrap_function',
-  testName: 'runTest'
-});
