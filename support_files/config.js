4d70336823a512945d044b28ee92c3dad0de2858
ghostboats <106226990+ghostboats@users.noreply.github.com>
2024-05-13 21:13:58 -0500

    the_jerinskis_update

--- support_files/config.js
+++ support_files/config.js
@@ -1,5 +1,6 @@
 const vscode = require('vscode');
 const path = require('path');
+const fs = require('fs');
 
 let config = {};
 
@@ -10,12 +11,13 @@ function setConfig(newConfig) {
 
 function getConfig() {
     const config = vscode.workspace.getConfiguration('bg3ModHelper');
+    const rootModPath = path.normalize(config.get('rootModPath'));
     return {
         maxFilesToShow: config.get('hover.maxFiles'),
         hoverEnabled: config.get('hover.enabled'),
         maxCacheSize: config.get('maxCacheSize'),
-        rootModPath: path.normalize(config.get('rootModPath')),
-        modName: path.basename(config.get('rootModPath')),
+        rootModPath: rootModPath,
+        modName: getModName(rootModPath),
         modDestPath: path.normalize(config.get('modDestPath')),
         lslibPath: path.normalize(config.get('lslibPath')),
         autoLaunchOnPack: config.get('autoLaunchOnPack'),
@@ -23,4 +25,18 @@ function getConfig() {
         excludedFiles: config.get('excludedFiles') || []
     };
 }
+
+function getModName(rootModPath) {
+    const modsPath = path.join(rootModPath, 'Mods');
+    try {
+        const directories = fs.readdirSync(modsPath, { withFileTypes: true })
+                            .filter(dirent => dirent.isDirectory())
+                            .map(dirent => dirent.name);
+        return directories[0] || ''; // Return the first directory name or an empty string if no directories are found
+    } catch (err) {
+        console.error(`Error reading directories from ${modsPath}: ${err}`);
+        return ''; // Return an empty string in case of an error
+    }
+}
+
 module.exports = { setConfig, getConfig };
