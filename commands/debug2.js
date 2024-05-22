29a1ef5246b1faee27b74b18ec983c798fdb716f
Ken Bynell <ken.bynell@gmail.com>
2024-05-22 01:42:54 -0500

    testing stuff

--- commands/debug2.js
+++ commands/debug2.js
@@ -12,24 +12,12 @@ const bg3mh_logger = CREATE_LOGGER();
 const debug2 = vscode.commands.registerCommand('bg3-mod-helper.debug2Command', async function () {
     const config = getConfig();
     const localizationPath = path.join(config.rootModPath, 'Localization');
-    let coreCount = os.availableParallelism();
+    let halfCoreCount = os.availableParallelism() / 2;
 
-    raiseInfo(`half of your cpu's cores: ${coreCount / 2}`);
+    raiseInfo(`half of your cpu's cores: ${halfCoreCount}`);
 
+    
 
-    if (isMainThread) {
-        const testWorker = new Worker(__filename);
-
-        console.log("in main thread");
-
-        testWorker.on('message', (msg) => {
-            console.log(msg);
-        });
-    }
-    else {
-        console.log("in worker thread");
-        parentPort.postMessage("hi");
-    }
 
     // after you are through with this command, the following line must be uncommented and the only thing left for this file to be considered cleaned up.
     // raiseInfo("hi dipshit :)");
