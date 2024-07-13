5f21f845aab2ddd5d5e9de9aea4c252a93e34482
Ken Bynell <ken.bynell@gmail.com>
2024-07-13 12:54:50 -0500

    rm nm p1

---
+++ node_modules/magickwand.js/test/integration/browser-webpack-esm/webpack.config.cjs
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  devServer: {
+    port: 8030,
+    static: {
+      directory: __dirname,
+    },
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  }
+};
